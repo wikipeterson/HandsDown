eee07656b13f19db459e3b54b93268d6e1167d5c
cwalter50 <walt50@gmail.com>
2018-01-09 15:30:42 -0500

    hello

--- HandsDown/ViewController.swift
+++ HandsDown/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.blue //That's right
-        
+        // hello
         //Steve is funny looking
     }
 
