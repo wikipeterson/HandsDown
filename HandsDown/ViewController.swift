613081830b4ecd7a4d6f521841af5b886592a980
cwalter50 <walt50@gmail.com>
2018-01-09 15:29:22 -0500

    added comment

--- HandsDown/ViewController.swift
+++ HandsDown/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.blue //That's right
+        
+        //Steve is funny looking
     }
 
     override func didReceiveMemoryWarning() {
